2d89b426af09d9cb4f1162f6a5af83d61e2a707b
Luciano Kodheli <lucianokodheli@Lucianos-iMac.local>
2024-08-07 16:56:18 -0400

    added the buttons

--- landing.js
+++ landing.js
@@ -1,1 +1,5 @@
 //code for the landing page and quote picker
+let  BOOK_API = 'AIzaSyAF346xBtlIrylYJvEP8dIP_L4581GQkOY'
+let getBook = $('#searchForm')
+let booksApi = `https://www.googleapis.com/books/v1/volumes?q=&key=${BOOK_API}`
+
